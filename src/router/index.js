2546de53afaafece93e825eb280eea48b9ece785
魏子涵 <muzhenweizh@gmail.com>
2022-07-03 22:25:15 +0800

    chore: 清除部分文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 // Components
-// import UserPage from '../pages/user/UserPage.vue'
-import ProfilePage from '../pages/user/profile/ProfilePage.vue'
 import UploadAvatarPage from '../pages/upload-avatar/UploadAvatarPage.vue'
 
 const routes = [
@@ -10,27 +8,11 @@ const routes = [
     path: '/',
     redirect: '/upload-avatar',
   },
-  // {
-  //   path: '/',
-  //   name: 'UserPage',
-  //   component: UserPage,
-  //   children: [
-  //     {
-  //       path: '/profile/:address',
-  //       name: 'ProfilePage',
-  //       component: ProfilePage,
-  //     },
-  //   ],
-  // },
+
   {
     path: '/upload-avatar',
     name: 'UploadAvatarPage',
     component: UploadAvatarPage,
-  },
-  {
-    path: '/profile/:address',
-    name: 'ProfilePage',
-    component: ProfilePage,
   },
 ]
 const router = createRouter({
